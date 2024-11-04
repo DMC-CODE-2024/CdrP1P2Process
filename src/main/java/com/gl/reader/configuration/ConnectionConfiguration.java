1601fbf49cc1526228d894e36b66b73a55e5679a
sachingitcode <me.yadavsachin7@gmail.com>
2024-11-04 17:51:46 +0700

    first commit

---
+++ src/main/java/com/gl/reader/configuration/ConnectionConfiguration.java
@@ -0,0 +1,26 @@
+package com.gl.reader.configuration;
+
+import org.springframework.orm.jpa.EntityManagerFactoryInfo;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.*;
+import jakarta.persistence.PersistenceContext;
+import java.sql.Connection;
+
+
+@Repository
+public class ConnectionConfiguration {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Connection getConnection() {
+        EntityManagerFactoryInfo info = (EntityManagerFactoryInfo) em.getEntityManagerFactory();
+        try {
+            return info.getDataSource().getConnection();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
